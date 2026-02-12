eaf75260c6fc405c4074e1db55d431faec381bce
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2026-02-12 23:31:19 +0100

    feat(extension-code-block-lowlight): add package with incremental highlighting, tab indent, autoDetect, SSR helper

---
+++ packages/extension-code-block-lowlight/src/index.ts
@@ -0,0 +1,10 @@
+export {
+  CodeBlockLowlight,
+  type CodeBlockLowlightOptions,
+  type CodeBlockLowlightStorage,
+} from './CodeBlockLowlight.js';
+export { lowlightPluginKey, type Lowlight } from './lowlightPlugin.js';
+export {
+  generateHighlightedHTML,
+  type GenerateHighlightedHTMLOptions,
+} from './generateHighlightedHTML.js';
