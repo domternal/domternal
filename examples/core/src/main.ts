8ef6d3df702bee9bb4069acb753c7a55638a2148
thomasnowhere <18348677+ThomasNowHere@users.noreply.github.com>
2026-03-17 15:57:55 +0100

    feat(examples): add Getting Started code to core example

--- examples/core/src/main.ts
+++ examples/core/src/main.ts
@@ -1,1 +1,7 @@
+import { Editor, StarterKit } from '@domternal/core';
 
+const editor = new Editor({
+  element: document.getElementById('editor')!,
+  extensions: [StarterKit],
+  content: '<p>Hello <strong>World</strong>!</p>',
+});
